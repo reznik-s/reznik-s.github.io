2f9eb010919a8552334a044ce51269ec7bdd507a
reznik-s <rezniksergey@list.ru>
2017-03-14 13:15:21 +0200

    Файлы для сайта

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var link = document.querySelector(".map__btn");
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content--close");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-content--show");
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content--show");
+});
